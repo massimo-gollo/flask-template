bfbb6e0a41ce5b95fe293c20fe88608c2605d047
massimogollo <massimo.gollo@hotmail.it>
2022-06-30 15:48:49 +0200

    add flask mongoengine integration

--- app/__init__.py
+++ app/__init__.py
@@ -6,6 +6,11 @@ def create_app(config=Config):
     app = Flask(__name__, instance_relative_config=False)
     app.config.from_object(config)
 
+    mongo.init_app(app)
+
+    from app.api import api_bp
+    app.register_blueprint(api_bp, url_prefix='/api')
+
     with app.app_context():
         from . import routes
 
--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 from os import environ
 
+
 class Config:
     # General Config
     SECRET_KEY = environ.get('SECRET_KEY')
     FLASK_APP = environ.get('FLASK_APP')
+
+    MONGODB_SETTINGS = {
+        'host': environ.get("MONGO_HOST"),
+        'db': environ.get("MONGO_NAME") or 'flask-template',
+        'username': environ.get("MONGO_USER"),
+        'password': environ.get("MONGO_PASS"),
+        'authentication_source': environ.get("MONGO_AUTH_SOURCE") or 'admin'
+    }
